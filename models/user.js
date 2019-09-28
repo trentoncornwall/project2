4de3e784ab9225fa42a1980c034345af057f7d0f
Trenton Cornwall <trenton434@gmail.com>
2019-09-28 11:30:56 -0600

    user.js eslint update

--- models/user.js
+++ models/user.js
@@ -1,9 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-        name: DataTypes.STRING,
-        password: DataTypes.STRING
-    });
-
-    return User;
-
+  var User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    password: DataTypes.STRINGp
+  });
+  return User;
 };
