f53cb5dc626da2da5d04f1278a8dc25d1f4a4015
amazingefren <efrentheroker@hotmail.com>
2019-09-28 13:47:46 -0600

    api route for utils

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,9 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all
   app.get("/api/utils", function(req, res) {
-    db.utilities.findAll({}).then(function(utils_db) {
-      res.json(utils_db);
+    db.Util.findAll({}).then(function(data) {
+      console.log(data);
+      res.json(data);
     });
   });
 };
