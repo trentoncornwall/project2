42f6ca0edddb99b48238667890282117d69b8ebe
Trenton Cornwall <trenton434@gmail.com>
2019-09-29 12:00:29 -0600

    edited bills.js, adding most routing, didn't get bills routing but should work

--- models/bills.js
+++ models/bills.js
@@ -1,40 +1,32 @@
 module.exports = function(sequelize, DataTypes) {
-    
-    var Bill = sequelize.define("Bill", {
-        userID: DataTypes.INTEGER,
-        utilityID: DataTypes.INTEGER,
-        amount: {
-                type: DataTypes.DECIMAL(6,2),
-                allowNull: false
-                },
-        billDate: {
-                type: DataTypes.DATEONLY,
-                allowNull: false
-                },
-        dueDate: {
-                type: DataTypes.DATEONLY,
-                allowNull: false
-                
-    });
+	var Bill = sequelize.define("Bill", {
+		amount: {
+			type: DataTypes.DECIMAL(6, 2),
+			allowNull: false
+		},
+		// billDate: {
+		// 	type: DataTypes.DATEONLY,
+		// 	allowNull: false
+		// },
+		dueDate: {
+			type: DataTypes.DATEONLY,
+			allowNull: false
+		}
+	});
 
-    Bill.associate = function(models) {
+	Bill.associate = function(models) {
+		Bill.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
 
-        Bill.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
+		Bill.belongsTo(models.Util, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
 
-        Bill.belongsTo(models.Util, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-
-    };
-
-    return Bill;
+	return Bill;
 };
-
-
-
