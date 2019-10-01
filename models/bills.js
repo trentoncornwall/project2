eb93bddbfdf6f9d6673c94f46052300a452026ad
Efren Castro <amazingefren@Efrens-iMac.local>
2019-09-30 21:10:58 -0600

    eslint format

--- models/bills.js
+++ models/bills.js
@@ -1,40 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-    
-    var Bill = sequelize.define("Bill", {
-        userID: DataTypes.INTEGER,
-        utilityID: DataTypes.INTEGER,
-        amount: {
-                type: DataTypes.DECIMAL(6,2),
-                allowNull: false
-                },
-        billDate: {
-                type: DataTypes.DATEONLY,
-                allowNull: false
-                },
-        dueDate: {
-                type: DataTypes.DATEONLY,
-                allowNull: false
-                
+  var Bill = sequelize.define("Bill", {
+    userID: DataTypes.INTEGER,
+    utilityID: DataTypes.INTEGER,
+    amount: {
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false
+    },
+    billDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    }
+  });
+
+  Bill.associate = function(models) {
+    Bill.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
     });
 
-    Bill.associate = function(models) {
+    Bill.belongsTo(models.Util, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
-        Bill.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-
-        Bill.belongsTo(models.Util, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-
-    };
-
-    return Bill;
+  return Bill;
 };
-
-
-
