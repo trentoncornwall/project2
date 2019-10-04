08caa6e4271b2eca14533401305f99aee56a8570
Trenton Cornwall <trenton434@gmail.com>
2019-10-03 20:56:21 -0600

    created a very basic tempalte for the home page, started login

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,10 +1,13 @@
 console.log("document ready pre jquery")
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("document ready jquery working")
-    $(".accordion").click(function(event){
+    $(".accordion").click(function (event) {
 
         $(this).parent().children(".panel").slideToggle();
         console.log("This is clicked");
-        
+
     });
+
+    $('.dropdown-trigger').dropdown();
+    $('.datepicker').datepicker();
 });
