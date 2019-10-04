0b829de5f16e251baaf770449dbba831e1949021
Trenton Cornwall <trenton434@gmail.com>
2019-10-03 21:16:36 -0600

    added some routing and stuff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,24 +1,27 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(youtils_db) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (youtils_db) {
       res.render("index");
     });
   });
 
-  app.get("/home", function(req, res) {
-    // db.Example.findAll({}).then(function(youtils_db) {
+  app.get("/home", function (req, res) {
+    const allUtils = db.Util.findAll({});
+    Promise.all([allUtils]).then(responses => {
       res.render("home");
-    // });
+      console.log(responses[0])
+    });
+
   });
 
 
-  
+
 
   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.render("404");
-//   });
+  //   app.get("*", function(req, res) {
+  //     res.render("404");
+  //   });
 };
