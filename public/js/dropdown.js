2be2e5d11ce67111849ab1dfd30731abe2b46337
Trenton Cornwall <trenton434@gmail.com>
2019-10-05 11:37:33 -0600

    moved the scope

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -11,13 +11,4 @@ $(document).ready(function () {
     $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker();
 
-
-    function onSignIn(googleUser) {
-        console.log("CALLED ONSIGNIN")
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
 });
