5b52ab99a2a2fe3959891008e21659a48d10f356
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-10-05 12:57:51 -0600

    cleaned up content and bill submit form

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,32 +1,24 @@
 console.log("document ready pre jquery")
 $(document).ready(function(){
     console.log("document ready jquery working")
+    
     $(".accordion").click(function(event){
 
         $(this).parent().children(".panel").slideToggle();
         console.log("This is clicked");
         
     });
+
+    $('.collapsible').collapsible();
+    $('.dropdown-trigger').dropdown();
+    $('.datepicker').datepicker();
+    $('select').formSelect();
+
+
+
+
 });
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
-  });
 
-  // Or with jQuery
 
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-
-   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
+  
