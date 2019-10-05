599cae681297af24b4d834609b0632f0b3e89b01
Trenton Cornwall <trenton434@gmail.com>
2019-10-05 13:01:24 -0600

    auth

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -11,18 +11,25 @@ $(document).ready(function () {
     $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker();
 
-    // var auth2 = gapi.auth2.getAuthInstance();
-    if (auth2.isSignedIn.get()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-        console.log('ID: ' + profile.getId());
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-    } else {
-        console.log('auth didnt work')
+
+    function onLoad() {
+        gapi.load('auth2', function () {
+            gapi.auth2.init();
+            if (auth2.isSignedIn.get()) {
+                var profile = auth2.currentUser.get().getBasicProfile();
+                console.log('ID: ' + profile.getId());
+                console.log('Full Name: ' + profile.getName());
+                console.log('Given Name: ' + profile.getGivenName());
+                console.log('Family Name: ' + profile.getFamilyName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail());
+            } else {
+                console.log('auth didnt work')
+            }
+        });
     }
+
+
 
 
 });
