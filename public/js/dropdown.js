84f0bb331f340cb2c92a04ff604b20abe0e67422
Trenton Cornwall <trenton434@gmail.com>
2019-10-05 12:52:38 -0600

    auth

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -11,4 +11,18 @@ $(document).ready(function () {
     $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker();
 
+    // var auth2 = gapi.auth2.getAuthInstance();
+    if (auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    } else {
+        console.log('auth didnt work')
+    }
+
+
 });
