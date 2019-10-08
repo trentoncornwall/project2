20a959f2a0d918fa247dcc92d63535d073a68113
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-10-08 17:47:42 -0600

    added total sum of bills

---
+++ routes/sumsRoutes.js
@@ -0,0 +1,30 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+ 
+    app.get("/api/total/user", (req, res) => {
+        db.Bill.sum("amount").then(data => {
+
+            console.log(`get request for total: ${data}`);
+            res.json(data);
+        })
+            
+    });
+
+
+
+
+};
+
+
+
+
+
+
+
+
+// User.findAll({ 
+//     limit: 10 ,
+//     order: 'follower DESC'
+// })
